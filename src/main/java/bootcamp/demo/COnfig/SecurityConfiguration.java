168cd5c7477d5151435c1b2fa4fd860f8a0806b8
KChu <kevinchu1337@gmail.com>
2017-08-23 14:13:31 -0400

    initalcommit

---
+++ src/main/java/bootcamp/demo/COnfig/SecurityConfiguration.java
@@ -0,0 +1,29 @@
+package bootcamp.demo.COnfig;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+       http.authorizeRequests()
+		.anyRequest().authenticated();
+		http
+		.formLogin()
+		.defaultSuccessUrl("/")
+        .and()
+		.httpBasic();
+
+	}
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication().withUser("user").password("password").roles("USER");
+    }
+
+}
--- src/main/java/bootcamp/demo/Controller/HomeController.java
+++ src/main/java/bootcamp/demo/Controller/HomeController.java
@@ -24,13 +24,6 @@ public class HomeController {
     public String index(Model model){
         return "index";
     }
-    @RequestMapping("/page1")
-    public String page1(Model model){
-        return "page1";
-    }
-    @RequestMapping("/page2")
-    public String page2(Model model){
-        return "page2";
-    }
+
 }
 
